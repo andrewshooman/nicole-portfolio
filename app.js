63de61b71364a587194f21f20b72b7b254b5e780
andrewshooman <57375522+andrewshooman@users.noreply.github.com>
2020-12-07 12:28:52 -0500

    updates

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
 app.engine('html', require('ejs').renderFile);
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
-exports.index = function(req, res){
-  res.render('index', { title: 'ejs' });};
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+router.get('/work', function(req, res, next) {
+  res.render('work', { title: 'Express' });
+});
 
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
+router.get('/work-single', function(req, res, next) {
+  res.render('work-single', { title: 'Express' });
+});
+
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/blog', function(req, res, next) {
+  res.render('blog', { title: 'Express' });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// exports.index = function(req, res){
+//   res.render('index', { title: 'ejs' });};
+
+module.exports = router;
 
